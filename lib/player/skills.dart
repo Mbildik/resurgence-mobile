9369bc25466ea30cb29e54d597ca6f9ff5b84123
haibbi <hibrahimbinol@gmail.com>
2021-01-10 16:38:20 +0300

    skill

--- lib/player/skills.dart
+++ lib/player/skills.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:resurgence/constants.dart';
 import 'package:resurgence/enum.dart';
 import 'package:resurgence/player/service.dart';
 import 'package:resurgence/ui/shared.dart';
@@ -23,49 +22,24 @@ class _SkillsState extends State<Skills> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Beceriler'),
-        actions: [
-          Tooltip(
-            message: S.help,
-            child: IconButton(
-              icon: Icon(Icons.info),
-              onPressed: () {
-                showHelpDialog(
-                  context: context,
-                  // todo improve help
-                  title: 'Beceriler',
-                  content: 'Ekranda tüm becerileri görebilirsin.\n'
-                      'Senin sahip olduğun becerilerin '
-                      'içerisinde değer yazar ve daha '
-                      'açık bir tonda gözükür.',
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      body: LoadingFutureBuilder<List<Skill>>(
-        future: this._skills,
-        onError: () => setState(() {
-          this._skills = this._playerService.skills();
-        }),
-        builder: (context, snapshot) {
-          var skills = snapshot.data;
+    return LoadingFutureBuilder<List<Skill>>(
+      future: this._skills,
+      onError: () => setState(() {
+        this._skills = this._playerService.skills();
+      }),
+      builder: (context, snapshot) {
+        var skills = snapshot.data;
 
-          var children =
-              skills.map((e) => _SkillInfo(e)).toList(growable: false);
+        var children = skills.map((e) => _SkillInfo(e)).toList(growable: false);
 
-          return GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 1.4,
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-            children: children,
-          );
-        },
-      ),
+        return GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.4,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          children: children,
+        );
+      },
     );
   }
 }
@@ -80,23 +54,18 @@ class _SkillInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const skillColor = const Color(0xFFC8786A);
-    // const skillColor = const Color(0xFF5F9DBF);
+    const skillColor = Colors.white;
     return Card(
-      color: Colors.grey[900],
-      shape: RoundedRectangleBorder(
-        side: const BorderSide(color: Colors.grey, width: 2.0),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
+            Icon(Icons.shield),
             FittedBox(
               child: Text(
                 skill.value,
-                style: Theme.of(context).textTheme.headline5.copyWith(
+                style: Theme.of(context).textTheme.headline6.copyWith(
                       fontWeight: FontWeight.bold,
                       color: skillColor,
                     ),
